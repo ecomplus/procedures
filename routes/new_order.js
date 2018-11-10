be8c75e3ed8ae173a9a2f98c76e11db1bd72667d
Leonardo Matos <leomp120894@gmail.com>
2018-11-09 22:14:22 -0200

    first endpoint (echo)

---
+++ routes/new_order.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// log on files
+// const logger = require('./../lib/Logger.js')
+
+const POST = (id, meta, body, respond) => {
+  respond('Hello')
+}
+
+export default {
+  POST
+}
