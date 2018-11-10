0c7436c549bcd4842467eb3da655815c67744671
Leonardo Matos <leomp120894@gmail.com>
2018-11-09 23:10:21 -0200

    using console-files package

--- routes/new_order.js
+++ routes/new_order.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // log on files
-// const logger = require('./../lib/Logger.js')
+// const logger = require('console-files')
 
 const POST = (id, meta, body, respond) => {
   respond('Hello')
