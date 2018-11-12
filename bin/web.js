9799da40c2afa5c71219d29db23c1bd9c2902253
Leonardo Matos <leomp120894@gmail.com>
2018-11-12 20:05:26 -0200

    checking sqlite setup error

--- bin/web.js
+++ bin/web.js
@@ -2,46 +2,50 @@
 
 // log on files
 const logger = require('console-files')
-// https://www.npmjs.com/package/rest-auto-router
-const restAutoRouter = require('rest-auto-router')
 
 // handle app authentication to Store API
 // https://github.com/ecomclub/ecomplus-auth-node
 const dbFilename = process.env.DB_FILENAME || process.cwd() + '/cpm.db'
-const apiAuth = require('ecomplus-app-auth')(dbFilename)
+require('ecomplus-app-auth')(dbFilename).catch(err => {
+  logger.error(err)
+})
 
-// web server configuration
-const conf = {
-  // path to routes folder
-  'path': process.cwd() + '/routes/',
-  // listened tcp port
-  // should be opened for localhost only
-  'port': parseInt(process.env.PROXY_PORT, 10) || 3000,
-  // part of the URL to be deleted in routing
-  // like RewriteBase of Apache Httpd mod_rewrite
-  'base_uri': process.env.PROXY_BASE_URI || '/api/v1/',
-  // must be configured in common with proxy server
-  'proxy': {
-    // request timeout in ms
-    'timeout': parseInt(process.env.PROXY_TIMEOUT, 10) || 30000,
-    // X-Authentication header
-    'auth': process.env.PROXY_AUTH || 'FnN3sT4'
-  },
-  // default error messages
-  // used when messages are null
-  'error_messages': {
-    'dev': 'Unknow error',
-    'usr': {
-      'en_us': 'Unexpected error, report to support or responsible developer',
-      'pt_br': 'Erro inesperado, reportar ao suporte ou desenvolvedor responsável'
-    }
-  },
-  // allow clients to specify what fields to receive from resource
-  // if true, response should vary by http param 'fields'
-  'vary_fields': false
-}
+.then(auth => {
+  // setup REST API server
+  // https://www.npmjs.com/package/rest-auto-router
+  const restAutoRouter = require('rest-auto-router')
 
-apiAuth.then(auth => {
+  // web server configuration
+  const conf = {
+    // path to routes folder
+    'path': process.cwd() + '/routes/',
+    // listened tcp port
+    // should be opened for localhost only
+    'port': parseInt(process.env.PROXY_PORT, 10) || 3000,
+    // part of the URL to be deleted in routing
+    // like RewriteBase of Apache Httpd mod_rewrite
+    'base_uri': process.env.PROXY_BASE_URI || '/api/v1/',
+    // must be configured in common with proxy server
+    'proxy': {
+      // request timeout in ms
+      'timeout': parseInt(process.env.PROXY_TIMEOUT, 10) || 30000,
+      // X-Authentication header
+      'auth': process.env.PROXY_AUTH || 'FnN3sT4'
+    },
+    // default error messages
+    // used when messages are null
+    'error_messages': {
+      'dev': 'Unknow error',
+      'usr': {
+        'en_us': 'Unexpected error, report to support or responsible developer',
+        'pt_br': 'Erro inesperado, reportar ao suporte ou desenvolvedor responsável'
+      }
+    },
+    // allow clients to specify what fields to receive from resource
+    // if true, response should vary by http param 'fields'
+    'vary_fields': false
+  }
+
   const middleware = (id, meta, body, respond, req, res, resource, verb, endpoint) => {
     // function called before endpoints
     // authentications and other prerequisites when necessary
