2f6ad35e85260c348b2b71fbd47c5aa634913b3e
Leonardo Matos <leomp120894@gmail.com>
2018-11-12 17:57:40 -0200

    using console-files v2

--- main.js
+++ main.js
@@ -8,27 +8,6 @@
  * @author E-Com Club
  */
 
-process.on('uncaughtException', (err) => {
-  // fatal error
-  // log to file before exit
-  let msg = '\n[' + new Date().toString() + ']\n'
-  if (err) {
-    if (err.hasOwnProperty('stack')) {
-      msg += err.stack
-    } else if (err.hasOwnProperty('message')) {
-      msg += err.message
-    } else {
-      msg += err.toString()
-    }
-    msg += '\n'
-  }
-
-  let fs = require('fs')
-  fs.appendFile('/var/log/nodejs/_stderr', msg, () => {
-    process.exit(1)
-  })
-})
-
 // web application
 // recieve requests from Nginx by reverse proxy
 require('./bin/web.js')
