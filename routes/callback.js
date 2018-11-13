a40f4dd140c69402e0c764d41821367ec8e091ed
Leonardo Matos <leomp120894@gmail.com>
2018-11-13 01:01:30 -0200

    start creating procedures if not already created

--- routes/callback.js
+++ routes/callback.js
@@ -5,25 +5,26 @@ const logger = require('console-files')
 
 const POST = (id, meta, body, respond, storeId, apiMethods) => {
   // Store API authentication callback
-  apiMethods.handleCallback(storeId, body).catch(err => {
+  apiMethods.handleCallback(storeId, body).then(({ isNew, authenticationId }) => {
+    // just respond first
+    respond(null, null, 204)
+
+    // handle successful authentication flux
+    if (!isNew) {
+      // not a new app installed
+      // authentication done
+      // create procedures if not already created
+      let auth = apiMethods.getAuth(storeId, authenticationId)
+      apiMethods.apiRequest(storeId, '/procedures.json', 'POST', {}, auth)
+    }
+  })
+
+  .catch(err => {
     if (typeof err.code === 'string' && !err.code.startsWith('SQLITE_CONSTRAINT')) {
       // debug SQLite errors
       logger.error(err)
     }
     respond({}, null, 500, 'cpm_callback_error', err.message)
-  })
-
-  // handle successful authentication flux
-  .then(({ isNew }) => {
-    // just respond first
-    respond(null, null, 204)
-    if (!isNew) {
-      // not a new app installed
-      // authentication done
-      // create procedures if not already created
-      let auth = apiMethods.getAuth(storeId, body.my_id)
-      apiMethods.apiRequest(storeId, '/procedures.json', 'POST', {}, auth)
-    }
   })
 }
 
