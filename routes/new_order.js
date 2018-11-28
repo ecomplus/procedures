43438e70cd752989fd81849257021e0b9ee178fd
Leonardo Matos <leomp120894@gmail.com>
2018-11-28 20:01:57 -0200

    debug trigger body

--- routes/new_order.js
+++ routes/new_order.js
@@ -1,10 +1,10 @@
 'use strict'
 
 // log on files
-// const logger = require('console-files')
+const logger = require('console-files')
 
 const POST = (id, meta, body, respond, storeId, appSdk) => {
-  console.log(body)
+  logger.log(body)
   respond({})
 }
 
