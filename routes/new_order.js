64264f1866a318761c0a5a4ae6dd1c63c9a95442
Leonardo Matos <leomp120894@gmail.com>
2018-11-29 12:24:37 -0200

    handling errors from api request with axios

--- routes/new_order.js
+++ routes/new_order.js
@@ -4,7 +4,33 @@
 const logger = require('console-files')
 
 const POST = (id, meta, body, respond, storeId, appSdk) => {
-  logger.log(body)
+  // logger.log(body)
+  let orderId = body.inserted_id
+  if (orderId) {
+    // GET order from API
+    let url = '/orders/' + orderId + '.json'
+    appSdk.apiRequest(storeId, url).then(order => {
+      // read configured options from app hidden data
+    })
+
+    .catch(err => {
+      if (err.response) {
+        let statusCode = err.response.status
+        if (statusCode >= 500 && statusCode < 600) {
+          // return error code to receive webhook again further
+          respond({}, null, statusCode, 'get_order_error_' + err.code, err.message)
+        } else {
+          // not found ?
+          // ignore
+          respond(err.message)
+        }
+      } else {
+        logger.error(err)
+        respond({}, null, 500, 'get_order_error', err.message)
+      }
+    })
+  }
+
   respond({})
 }
 
