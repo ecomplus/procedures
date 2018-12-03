0cb7e24f551edcf2d0823dc4586738dbd3ddbda5
Leonardo Matos <leomp120894@gmail.com>
2018-12-03 16:56:06 -0200

    fix tables

--- assets/tables.sql
+++ assets/tables.sql
@@ -1,9 +1,19 @@
-CREATE TABLE IF NOT EXISTS `stock_control` (
+CREATE TABLE IF NOT EXISTS `order_items` (
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `store_id` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+  `order_id` CHAR(24) NOT NULL,
+  `item_id` CHAR(24) NULL,
   `product_id` CHAR(24) NOT NULL,
   `variation_id` CHAR(24) NOT NULL,
-  `last_read_quantity` DECIMAL(11,4) NOT NULL DEFAULT 0,
-  `last_read_record` CHAR(24) NULL,
-  PRIMARY KEY (`product_id`, `variation_id`)
+  `quantity` DECIMAL(11,4) NOT NULL DEFAULT 0,
+  `price` DECIMAL(14,5) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`order_id`, `product_id`, `variation_id`)
+) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `order_buyers` (
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `store_id` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+  `order_id` CHAR(24) NOT NULL,
+  `customer_id` CHAR(24) NOT NULL,
+  PRIMARY KEY (`order_id`, `customer_id`)
 ) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_unicode_ci;
