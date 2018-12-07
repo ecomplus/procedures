8a0193f641e2a8537035f8e56acf3dc05c66790f
Leonardo Matos <leomp120894@gmail.com>
2018-12-07 13:23:00 -0200

    handle customer endpoint to fix orders

--- routes/customer.js
+++ routes/customer.js
@@ -8,11 +8,7 @@ const errorResponse = require('./#error')()
 const triggerParse = require(process.cwd() + '/lib/Api/TriggerParse')
 
 // procedure functions
-const BuyersAdd = require(process.cwd() + '/lib/Api/Orders/Buyers/Add')
-const BuyersRemove = require(process.cwd() + '/lib/Api/Orders/Buyers/Remove')
-const ItemsAdd = require(process.cwd() + '/lib/Api/Orders/Items/Add')
-const ItemsRemove = require(process.cwd() + '/lib/Api/Orders/Items/Remove')
-const StatusFix = require(process.cwd() + '/lib/Api/Orders/Status/Fix')
+const OrdersFix = require(process.cwd() + '/lib/Api/Customers/Orders/Fix')
 
 const POST = (id, meta, trigger, respond, storeId, appSdk) => {
   const { object, objectId } = triggerParse(trigger)
@@ -20,63 +16,10 @@ const POST = (id, meta, trigger, respond, storeId, appSdk) => {
     // get authentication tokens
     appSdk.getAuth(storeId).then(auth => {
       const client = { appSdk, storeId, auth }
-      let resCode = 1
-
-      // check trigger action to proceed with functions
-      let handleFix = true
-      switch (trigger.action) {
-        case 'create':
-          // new order or nested objects subresource
-          // same handler
-          // check for new order items and buyers
-          ItemsAdd(client, object)
-            .then(BuyersAdd)
-          resCode = 101
-          break
-
-        case 'change':
-          if (!trigger.subresource) {
-            // order partially edited or entire reseted
-            // removeAll = true
-            BuyersRemove(client, object, true)
-              .then(BuyersAdd)
-              .then(ItemsRemove)
-              .then(ItemsAdd)
-            resCode = 102
-          } else {
-            // check for specific order item partially edited
-            // also check for status chages
-            // ignore specific buyer edition if any (nothing to do)
-            ItemsRemove(client, object)
-              .then(ItemsAdd)
-            resCode = 103
-          }
-          break
-
-        case 'delete':
-          if (!trigger.subresource) {
-            // order removed
-            // removeAll = true
-            BuyersRemove(client, object, true)
-              .then(ItemsRemove)
-            resCode = 105
-            handleFix = false
-          } else {
-            // check for specific order item or buyer removed
-            BuyersRemove(client, object)
-              .then(ItemsRemove)
-            resCode = 106
-          }
-          break
-      }
-
-      if (handleFix) {
-        // fix payment, shipping and order status if relevant changes were made
-        StatusFix(client, object)
-      }
+      OrdersFix(client, object)
 
       // end current request with success
-      respond(resCode)
+      respond(201)
     }).catch(err => {
       errorResponse(err, respond)
     })
